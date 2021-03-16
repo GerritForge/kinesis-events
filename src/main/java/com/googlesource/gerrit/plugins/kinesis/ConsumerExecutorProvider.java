77b84c616c90d61a13464957fe6617629263094e
Antonio Barone <syntonyze@gmail.com>
2021-03-16 21:06:48 +0100

    Initial implementation of kinesis-events
    Change-Id: Ic88d5254438187f1d59c3981854fe7acacc5eeae

---
+++ src/main/java/com/googlesource/gerrit/plugins/kinesis/ConsumerExecutorProvider.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.kinesis;
+
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.concurrent.ExecutorService;
+
+class ConsumerExecutorProvider implements Provider<ExecutorService> {
+  private final WorkQueue workQueue;
+  private final KinesisConfiguration configuration;
+
+  @Inject
+  ConsumerExecutorProvider(WorkQueue workQueue, KinesisConfiguration configuration) {
+    this.workQueue = workQueue;
+    this.configuration = configuration;
+  }
+
+  @Override
+  public ExecutorService get() {
+    return workQueue.createQueue(configuration.getNumberOfSubscribers(), "kinesis-consumers");
+  }
+}
