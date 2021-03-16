89f924d98e3098731a5a0fe3d5341245c68eb8e7
Antonio Barone <syntonyze@gmail.com>
2021-03-16 21:06:48 +0100

    Initial implementation of kinesis-events
    Change-Id: Ic88d5254438187f1d59c3981854fe7acacc5eeae

---
+++ src/main/java/com/googlesource/gerrit/plugins/kinesis/KinesisBrokerApi.java
@@ -0,0 +1,78 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.kinesis;
+
+import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.EventMessage;
+import com.gerritforge.gerrit.eventbroker.TopicSubscriber;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+class KinesisBrokerApi implements BrokerApi {
+  private final KinesisConsumer.Factory consumerFactory;
+
+  private final List<KinesisConsumer> consumers;
+  private final Gson gson;
+  private final KinesisPublisher kinesisPublisher;
+
+  @Inject
+  public KinesisBrokerApi(
+      Gson gson, KinesisPublisher kinesisPublisher, KinesisConsumer.Factory consumerFactory) {
+    this.gson = gson;
+    this.kinesisPublisher = kinesisPublisher;
+    this.consumerFactory = consumerFactory;
+    this.consumers = new ArrayList<>();
+  }
+
+  @Override
+  public boolean send(String streamName, EventMessage event) {
+    return kinesisPublisher.publish(
+        streamName, gson.toJson(event), event.getHeader().sourceInstanceId.toString());
+  }
+
+  @Override
+  public void receiveAsync(String streamName, Consumer<EventMessage> eventConsumer) {
+    KinesisConsumer consumer = consumerFactory.create(streamName, eventConsumer);
+    synchronized (consumers) {
+      consumers.add(consumer);
+    }
+    consumer.subscribe(streamName, eventConsumer);
+  }
+
+  @Override
+  public Set<TopicSubscriber> topicSubscribers() {
+    return consumers.stream()
+        .map(s -> TopicSubscriber.topicSubscriber(s.getStreamName(), s.getMessageProcessor()))
+        .collect(Collectors.toSet());
+  }
+
+  @Override
+  public void disconnect() {
+    consumers.forEach(KinesisConsumer::shutdown);
+    consumers.clear();
+  }
+
+  @Override
+  public void replayAllEvents(String topic) {
+    consumers.stream()
+        .filter(subscriber -> topic.equals(subscriber.getStreamName()))
+        .forEach(KinesisConsumer::resetOffset);
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/kinesis/KinesisEventsIT.java
@@ -0,0 +1,112 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.kinesis;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.CLOUDWATCH;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.KINESIS;
+
+import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.EventMessage;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.WaitUtil;
+import com.google.gerrit.acceptance.config.GerritConfig;
+import com.google.gerrit.server.events.ProjectCreatedEvent;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kinesis.KinesisClient;
+import software.amazon.awssdk.services.kinesis.model.CreateStreamRequest;
+
+@TestPlugin(name = "kinesis-events", sysModule = "com.googlesource.gerrit.plugins.kinesis.Module")
+public class KinesisEventsIT extends LightweightPluginDaemonTest {
+
+  private static final int LOCALSTACK_PORT = 4566;
+  private LocalStackContainer localstack =
+      new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.8"))
+          .withServices(DYNAMODB, KINESIS, CLOUDWATCH)
+          .withExposedPorts(LOCALSTACK_PORT);
+
+  private KinesisClient kinesisClient;
+
+  @Before
+  public void setUpTestPlugin() throws Exception {
+    localstack.start();
+
+    kinesisClient =
+        KinesisClient.builder()
+            .endpointOverride(localstack.getEndpointOverride(KINESIS))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(
+                        localstack.getAccessKey(), localstack.getSecretKey())))
+            .region(Region.of(localstack.getRegion()))
+            .build();
+
+    System.setProperty("endpoint", localstack.getEndpointOverride(KINESIS).toASCIIString());
+    System.setProperty("region", localstack.getRegion());
+    System.setProperty("aws.accessKeyId", localstack.getAccessKey());
+    System.setProperty("aws.secretAccessKey", localstack.getSecretKey());
+
+    super.setUpTestPlugin();
+  }
+
+  @Override
+  public void tearDownTestPlugin() {
+    localstack.close();
+
+    super.tearDownTestPlugin();
+  }
+
+  @Test
+  @GerritConfig(name = "plugin.kinesis-events.applicationName", value = "test-consumer")
+  @GerritConfig(name = "plugin.kinesis-events.initialPosition", value = "trim_horizon")
+  public void shouldConsumeAnEventPublishedToATopic() throws Exception {
+    String streamName = UUID.randomUUID().toString();
+    createKinesisStream(streamName);
+    // This timeout is quite high to allow the kinesis coordinator to acquire a
+    // lease on the newly created stream
+    Duration WAIT_FOR_CONSUMPTION = Duration.ofSeconds(120);
+
+    List<EventMessage> consumedMessages = new ArrayList<>();
+    kinesisBroker().receiveAsync(streamName, consumedMessages::add);
+
+    kinesisBroker().send(streamName, eventMessage());
+    WaitUtil.waitUntil(() -> consumedMessages.size() > 0, WAIT_FOR_CONSUMPTION);
+  }
+
+  public KinesisBrokerApi kinesisBroker() {
+    return (KinesisBrokerApi) plugin.getSysInjector().getInstance(BrokerApi.class);
+  }
+
+  private void createKinesisStream(String streamName) {
+    kinesisClient.createStream(
+        CreateStreamRequest.builder().streamName(streamName).shardCount(1).build());
+  }
+
+  private EventMessage eventMessage() {
+    return new EventMessage(
+        new EventMessage.Header(UUID.randomUUID(), UUID.randomUUID()), new ProjectCreatedEvent());
+  }
+}
