9f63cdd0239b6bcf9c78312d3c8dd5213a1c36ec
Antonio Barone <syntonyze@gmail.com>
2021-03-29 21:24:53 +0200

    Fix flaky test
    Wait for kinesis stream to be in status ACTIVE before attempting to
    ingest data.
    
    Change-Id: Ie82dbaa14d485784c9918840e7c4b9aee67b574a

--- src/test/java/com/googlesource/gerrit/plugins/kinesis/KinesisEventsIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/kinesis/KinesisEventsIT.java
@@ -38,9 +38,15 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.awssdk.services.kinesis.model.CreateStreamRequest;
+import software.amazon.awssdk.services.kinesis.model.DescribeStreamRequest;
+import software.amazon.awssdk.services.kinesis.model.StreamStatus;
 
 @TestPlugin(name = "kinesis-events", sysModule = "com.googlesource.gerrit.plugins.kinesis.Module")
 public class KinesisEventsIT extends LightweightPluginDaemonTest {
+  // This timeout is quite high to allow the kinesis coordinator to acquire a
+  // lease on the newly created stream
+  private static final Duration WAIT_FOR_CONSUMPTION = Duration.ofSeconds(120);
+  private static final Duration STREAM_CREATION_TIMEOUT = Duration.ofSeconds(10);
 
   private static final int LOCALSTACK_PORT = 4566;
   private LocalStackContainer localstack =
@@ -84,15 +90,13 @@ public class KinesisEventsIT extends LightweightPluginDaemonTest {
   @GerritConfig(name = "plugin.kinesis-events.initialPosition", value = "trim_horizon")
   public void shouldConsumeAnEventPublishedToATopic() throws Exception {
     String streamName = UUID.randomUUID().toString();
-    createKinesisStream(streamName);
-    // This timeout is quite high to allow the kinesis coordinator to acquire a
-    // lease on the newly created stream
-    Duration WAIT_FOR_CONSUMPTION = Duration.ofSeconds(120);
+    createKinesisStream(streamName, STREAM_CREATION_TIMEOUT);
 
     List<EventMessage> consumedMessages = new ArrayList<>();
     kinesisBroker().receiveAsync(streamName, consumedMessages::add);
 
     kinesisBroker().send(streamName, eventMessage());
+
     WaitUtil.waitUntil(() -> consumedMessages.size() > 0, WAIT_FOR_CONSUMPTION);
   }
 
@@ -100,9 +104,19 @@ public class KinesisEventsIT extends LightweightPluginDaemonTest {
     return (KinesisBrokerApi) plugin.getSysInjector().getInstance(BrokerApi.class);
   }
 
-  private void createKinesisStream(String streamName) {
+  private void createKinesisStream(String streamName, Duration timeout)
+      throws InterruptedException {
     kinesisClient.createStream(
         CreateStreamRequest.builder().streamName(streamName).shardCount(1).build());
+
+    WaitUtil.waitUntil(
+        () ->
+            kinesisClient
+                .describeStream(DescribeStreamRequest.builder().streamName(streamName).build())
+                .streamDescription()
+                .streamStatus()
+                .equals(StreamStatus.ACTIVE),
+        timeout);
   }
 
   private EventMessage eventMessage() {
