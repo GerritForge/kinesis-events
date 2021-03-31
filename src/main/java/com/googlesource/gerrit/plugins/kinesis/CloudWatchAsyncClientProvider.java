4dc8851e60a60f04e24ee07805c66946fd4c3b63
Antonio Barone <syntonyze@gmail.com>
2021-03-31 18:12:56 +0200

    Refactor kinesis configuration
    Narrow responsibility of KinesisConfiguration to only return file based
    configuration values and extract AWS client creations to separate
    providers.
    
    Change-Id: I250c57faf7d9613cfcccba0b84c0fac7c05d2792

---
+++ src/main/java/com/googlesource/gerrit/plugins/kinesis/CloudWatchAsyncClientProvider.java
@@ -0,0 +1,40 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.kinesis;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClientBuilder;
+
+@Singleton
+class CloudWatchAsyncClientProvider implements Provider<CloudWatchAsyncClient> {
+  private final KinesisConfiguration kinesisConfiguration;
+
+  @Inject
+  CloudWatchAsyncClientProvider(KinesisConfiguration kinesisConfiguration) {
+    this.kinesisConfiguration = kinesisConfiguration;
+  }
+
+  @Override
+  public CloudWatchAsyncClient get() {
+    CloudWatchAsyncClientBuilder builder = CloudWatchAsyncClient.builder();
+    kinesisConfiguration.getRegion().ifPresent(builder::region);
+    kinesisConfiguration.getEndpoint().ifPresent(builder::endpointOverride);
+
+    return builder.build();
+  }
+}
